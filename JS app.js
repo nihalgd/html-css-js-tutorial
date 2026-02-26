986510e250bc2e7204e4e9578e93df36084dd4d0
nihalgd <nihalgoud.12345@gmail.com>
2026-02-26 17:29:20 +0530

    Add initial JavaScript, HTML, and CSS files for project setup

---
+++ JS app.js
@@ -0,0 +1,34 @@
+// console.log("Hello Javascript!");
+// console.log("Nihal Goud!");
+
+// let a = 5;
+// let b = 12;
+
+// console.log(a++);
+// console.log(++a);
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b); 
+
+// let age = 21;
+// console.log(age<18)
+// console.log(age>18)
+// console.log(age<=18)
+// console.log(age>=18)
+// console.log(age==18)
+// console.log(age!=18)
+
+//conditionals statements
+let colr = "red";
+if(colr == "red") {
+    console.log("stop");
+}
+else if(colr =s= "yellow") {
+    console.log("ready");
+}
+else
+    console.log("go");
