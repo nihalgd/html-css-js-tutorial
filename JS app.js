ccfeb3f1556b97058004c8d1d5b3c8a8f773a2c4
nihalgd <nihalgoud.12345@gmail.com>
2026-02-27 17:02:44 +0530

    Refactor and comment out conditional statements and additional code examples in JS app.js

--- JS app.js
+++ JS app.js
@@ -23,12 +23,91 @@
 // console.log(age!=18)
 
 //conditionals statements
-let colr = "red";
-if(colr == "red") {
-    console.log("stop");
-}
-else if(colr =s= "yellow") {
-    console.log("ready");
-}
-else
-    console.log("go");
+// let colr = "green";
+// if(colr == "red") {
+//     console.log("stop");
+// }
+// if(colr == "yellow") {
+//     console.log("ready");
+// }
+// else
+// // if(colr == "green")
+// {
+//     console.log("gooo");
+// }
+
+// let age = 19;
+// if(age >= 18){
+//     console.log("you can drive & vote");
+// }
+// else if(age < 18){
+//      console.log("you cannot drive & vote");
+// }
+
+// let size = "L"
+// if(size == "XL"){
+//     console.log("price is Rs. 250");
+// }
+// else if(size == "L"){
+//     console.log("price is Rs. 200");
+// }
+// else if(size == "M"){
+//     console.log("price is Rs. 100");
+// }
+// else{
+//     console.log("price is Rs. 50");
+// }
+
+// let marks = 83;
+// if(marks >= 33){
+//     console.log("pass");
+//     if(marks>80){
+//     console.log("Grade A+");
+//     }
+//     else{
+//         console.log("Grade C+");
+//     }
+// }
+// else{
+//     console.log("fail")
+// }
+
+
+// let marks = 83;
+// if(marks >= 33 && marks>80){
+//     console.log("pass");
+//     console.log("A+");
+// }
+// if(marks >= 33 || marks>80){
+//     console.log("pass");
+//     console.log("A+");
+// }
+// if(!marks < 33){
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+// let color = "yellow";
+// switch(color){
+//     case "red" :
+//     console.log("stop");
+//     break;
+//     case "yellow" :
+//     console.log("ready");
+//     break;
+//     case "green" :
+//     console.log("Go");
+//     break;
+//     default :
+//     console.log("bhaad meh jao");
+// }
+// alert("network error")
+// let name = prompt("enter your name : ");
+// console.log(name);
+
+// let userName = "      Nihal Goud       ";
+// let newname = userName.trim().toUpperCase();
+// console.log(newname);
+
+// let user = "NIHAL GOUD";
+// console.log(user.slice(0,5));
