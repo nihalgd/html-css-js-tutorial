8c39cbdd8deb84f137835d3d323ed4d68ac9344c
nihalgd <nihalgoud.12345@gmail.com>
2026-03-07 00:09:45 +0530

    Learn Javascript

--- JS 3 app.js
+++ JS 3 app.js
@@ -1,69 +1,89 @@
-// // DOM 
+// // // DOM 
 
-// let abcd = document.getElementById("abcd");
-// console.dir(abcd);
+// // let abcd = document.getElementById("abcd");
+// // console.dir(abcd);
 
-// document.getElementsByClassName("abcd");
-// console.log(abcd);
+// // document.getElementsByClassName("abcd");
+// // console.log(abcd);
 
-// let h1 = document.querySelector ("h1")
-// h1.textContent = ("hellloooowiiiiii");
-// h1.innerHTML = ("hellow!"); // yeh html meh dalta haiii
-// console.dir(h1);
+// // let h1 = document.querySelector ("h1")
+// // h1.textContent = ("hellloooowiiiiii");
+// // h1.innerHTML = ("hellow!"); // yeh html meh dalta haiii
+// // console.dir(h1);
 
-// // Attribute Manipulation (get set remove)
+// // // Attribute Manipulation (get set remove)
 
-// let a = document.querySelector("a")
-// console.log(a.getAttribute("href"));
-// // console.log(a.removeAttribute("href"));
+// // let a = document.querySelector("a")
+// // console.log(a.getAttribute("href"));
+// // // console.log(a.removeAttribute("href"));
 
-// // Dynamic DOM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// // // Dynamic DOM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-// let newh1 = document.createElement("h1")
-// h1.textContent = ("hellooww jii");
-// document.body.appendChild(h1);
-// console.log(h1);
+// // let newh1 = document.createElement("h1")
+// // h1.textContent = ("hellooww jii");
+// // document.body.appendChild(h1);
+// // console.log(h1);
 
-// h3 = document.querySelector("h3");
-// h3.remove();
-// console.dir(h3);
+// // h3 = document.querySelector("h3");
+// // h3.remove();
+// // console.dir(h3);
 
 
-// // addd styling from css - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// // // addd styling from css - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-// let ok = document.getElementById("ok");
-// ok.style.color = "red";
-// ok.style.backgroundColor = "yellow"
-// console.dir(ok);
+// // let ok = document.getElementById("ok");
+// // ok.style.color = "red";
+// // ok.style.backgroundColor = "yellow"
+// // console.dir(ok);
 
-// EVENT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// // EVENT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// // let button = document.querySelectorAll("button");
+// // console.dir(button);
+
+// // for (button of button){
+// //     button.onmouseenter = function(){
+// //     console.log("button was clicked!")
+// // };
+// // }
+
+// // button.onclick = function(){
+// //     console.log("button was clicked!")
+// // };
+
 
 // let button = document.querySelectorAll("button");
-// console.dir(button);
 
 // for (button of button){
-//     button.onmouseenter = function(){
-//     console.log("button was clicked!")
-// };
+// //    button.addEventListener("click" , saymyname);
+// //    button.addEventListener("click" , saymynaam);
+//    button.addEventListener("dblclick" , saymyname);
+//    button.addEventListener("dblclick" , saymynaam);
+//    console.log("double click!")
+// }
+// function saymyname(){
+//     alert("walter white");
+// }
+// function saymynaam(){
+//     alert("Pablo");
 // }
 
-// button.onclick = function(){
-//     console.log("button was clicked!")
-// };
+// let btn = document.querySelector("button");
 
 
-let button = document.querySelectorAll("button");
+// btn.addEventlistener("click", function(){
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerText = randomColor;
+//     console.log("Generate random color")
+// })
 
-for (button of button){
-    button.onclick = saymyname;
-    button.onclick = saymynaam;
+// function getRandomColor (){
+//     let red = Math.floor(Math.random()*255);
+//     let green = Math.floor(Math.random()*255);
+//     let blue = Math.floor(Math.random()*255);
 
-}
-function saymyname(){
-    alert("walter white");
-}
-function saymynaam(){
-    alert("Pablo");
-}
-
-
+//     let color = `(${red} , ${green} , ${blue} ,)`;
+//     return color;
+// }
+// // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
