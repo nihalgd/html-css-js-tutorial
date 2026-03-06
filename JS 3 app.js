3db1807d2871ced2a819d5113f654bd2c0db7dee
nihalgd <nihalgoud.12345@gmail.com>
2026-03-06 23:24:54 +0530

    Learn Javascript

--- JS 3 app.js
+++ JS 3 app.js
@@ -1,29 +1,69 @@
-// DOM 
+// // DOM 
 
-let abcd = document.getElementById("abcd");
-console.dir(abcd);
+// let abcd = document.getElementById("abcd");
+// console.dir(abcd);
 
-document.getElementsByClassName("abcd");
-console.log(abcd);
+// document.getElementsByClassName("abcd");
+// console.log(abcd);
 
-let h1 = document.querySelector ("h1")
-h1.textContent = ("hellloooowiiiiii");
-h1.innerHTML = ("hellow!"); // yeh html dalta haiii
-console.dir(h1);
+// let h1 = document.querySelector ("h1")
+// h1.textContent = ("hellloooowiiiiii");
+// h1.innerHTML = ("hellow!"); // yeh html meh dalta haiii
+// console.dir(h1);
 
-// Attribute Manipulation (get set remove)
+// // Attribute Manipulation (get set remove)
 
-let a = document.querySelector("a")
-console.log(a.getAttribute("href"));
-// console.log(a.removeAttribute("href"));
+// let a = document.querySelector("a")
+// console.log(a.getAttribute("href"));
+// // console.log(a.removeAttribute("href"));
 
-// Dynamic DOM
+// // Dynamic DOM - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 // let newh1 = document.createElement("h1")
 // h1.textContent = ("hellooww jii");
 // document.body.appendChild(h1);
 // console.log(h1);
 
-let h3 = document.querySelector("h3");
-h3.remove();
-console.log(h3);
+// h3 = document.querySelector("h3");
+// h3.remove();
+// console.dir(h3);
+
+
+// // addd styling from css - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// let ok = document.getElementById("ok");
+// ok.style.color = "red";
+// ok.style.backgroundColor = "yellow"
+// console.dir(ok);
+
+// EVENT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// let button = document.querySelectorAll("button");
+// console.dir(button);
+
+// for (button of button){
+//     button.onmouseenter = function(){
+//     console.log("button was clicked!")
+// };
+// }
+
+// button.onclick = function(){
+//     console.log("button was clicked!")
+// };
+
+
+let button = document.querySelectorAll("button");
+
+for (button of button){
+    button.onclick = saymyname;
+    button.onclick = saymynaam;
+
+}
+function saymyname(){
+    alert("walter white");
+}
+function saymynaam(){
+    alert("Pablo");
+}
+
+
