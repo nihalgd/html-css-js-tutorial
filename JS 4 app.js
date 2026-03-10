b245fbc2a99ff7042920d0e8ed39338e2a4f40de
nihalgd <nihalgoud.12345@gmail.com>
2026-03-10 16:55:21 +0530

    Learn Javascript

--- JS 4 app.js
+++ JS 4 app.js
@@ -6,4 +6,37 @@ localStorage.setItem("name" , "NIHAL GOUD");
 sessionStorage.setItem("name" , "NIHAL GOUD");
 
 // Cookies
-cookieStore
+document.cookie = ("name" , "NIHAL GOUD");
+
+localStorage.setItem("friends", JSON.stringify(["nihal" , "gourav" , "aryan"]))
+// JSON.parse()
+
+// dark-light mode
+// window.matchMedia('(prefers-color-scheme: dark)')
+
+function setDarkOrLight() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    }
+}
+
+setDarkOrLight();
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    if (document.body.classList.contains("dark")) {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+    } else {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    }
+});
+
+// CALLBACK HELL
